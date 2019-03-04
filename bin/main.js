6221106244729f58da495d1e6a4423c8ce15ba1b
Frank Oh <frankyoh@tmon.co.kr>
2019-03-04 14:14:49 +0900

    reformat codes

--- bin/main.js
+++ bin/main.js
@@ -1,64 +1,64 @@
 #!/usr/bin/env node
 
 var Pomodoro = require(__dirname + "/../lib/pomodoro").Pomodoro
-  , nomnom = require('nomnom');
+    , nomnom = require('nomnom');
 
 var options = nomnom.options({
     task: {
-      position: 0,
-      help: "This is the task you are going to do. When the pomodoro is" +
-        "complete it will say the time is done on that task",
+        position: 0,
+        help: "This is the task you are going to do. When the pomodoro is" +
+            "complete it will say the time is done on that task",
     }
     , add: {
-      abbr: 'a'
-      , help: "Add a new item that can be processed later"
+        abbr: 'a'
+        , help: "Add a new item that can be processed later"
     }
     , remove: {
-      abbr: 'r'
-      , help: "Remove items that we have saved to disk"
+        abbr: 'r'
+        , help: "Remove items that we have saved to disk"
     }
     , show: {
-      flag: true
-      , abbr: 's'
-      , help: "Show all the items in the file"
+        flag: true
+        , abbr: 's'
+        , help: "Show all the items in the file"
     }
     , start: {
-      flag: true
-      , help: "Start running the top item in the list"
+        flag: true
+        , help: "Start running the top item in the list"
     }
     , break: {
-      abbr: 'b'
-      , help: "Time a break and be notified when to start again. Pass in the" +
-        " amount of munites to break for."
+        abbr: 'b'
+        , help: "Time a break and be notified when to start again. Pass in the" +
+            " amount of munites to break for."
     }
     , version: {
-      flag: true
-      , abbr: 'v'
-      , help: "writes the version to the screen"
+        flag: true
+        , abbr: 'v'
+        , help: "writes the version to the screen"
     }
-  }).parse();
+}).parse();
 
 var pomodoro = new Pomodoro();
 if (!options.add && !options.remove && !options.show && !options.start && !options.break) {
-  pomodoro.run(options._.join(' '));
+    pomodoro.run(options._.join(' '));
 }
 
 if (options.add) {
-  pomodoro.add(options.add + ' ' + options._.join(' '));
+    pomodoro.add(options.add + ' ' + options._.join(' '));
 }
 
 if (options.remove) {
-  pomodoro.remove(options.remove);
+    pomodoro.remove(options.remove);
 }
 
 if (options.show) {
-  pomodoro.show();
+    pomodoro.show();
 }
 
 if (options.start) {
-  pomodoro.start();
+    pomodoro.start();
 }
 
 if (options.break) {
-  pomodoro.break(options.break);
+    pomodoro.break(options.break);
 }
